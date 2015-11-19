4a288ad99b7519161cba2c06bb788e7a191eab22
David Susco <dsusco@gmail.com>
2015-11-19 14:27:47 -0500

    added local nav, new index and tag index page generators, layouts and updated sidebar

--- _site/_plugins/generators.rb
+++ _site/_plugins/generators.rb
@@ -3,9 +3,20 @@ module Jekyll
   require 'less'
   require 'uglifier'
 
+  class DocumentLocalNavWeightGenerator < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      site.documents.each do |document|
+        document.data['local_nav_weight'] = document.data['date'].to_i * -1
+      end
+    end
+  end
+
   class JavascriptGenerator < Generator
     safe true
-    priority :high
+    priority :highest
 
     def generate(site)
       uglifier_options = (site.config['uglifier'] or {}).symbolize_keys
@@ -23,7 +34,7 @@ module Jekyll
 
   class LessGenerator < Generator
     safe true
-    priority :high
+    priority :highest
 
     def generate(site)
       css_options = site.config['css'] or {}
@@ -36,6 +47,104 @@ module Jekyll
           page.content = parser.parse(page.content).to_css(css_options)
           page.data['layout'] = nil
           page.ext = '.css'
+        end
+      end
+    end
+  end
+
+  class NewsIndexPageGenerator < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      index_pages = {}
+
+      site.documents.each do |document|
+        dir = '/'
+        title = nil
+
+        File.dirname(document.url).split(/\//).slice(1..-1).each do |token|
+          dir += token + '/'
+
+          if index_pages["#{dir}index.html"]
+            index_pages["#{dir}index.html"].data['documents'].unshift(document)
+          else
+            short_title = token.split.map(&:capitalize).join(' ') or token.capitalize
+
+            if (title =~ /\d$/) and (short_title =~ /^\d+$/)
+              title = title + '-' + short_title rescue short_title
+            else
+              title = title + ' ' + short_title rescue short_title
+            end
+
+            index = NewsIndexPage.new(
+              site,
+              dir,
+              { description: "#{site.config['title']} #{title.downcase.gsub(/(news)(.*)/, '\2 \1').strip} listing: keep up to date on the latest #{site.config['title']} news.",
+                short_title: short_title,
+                title: title
+              }
+            )
+
+            index_pages[index.url] = index
+            index_pages[index.url].data['documents'] = [document]
+          end
+        end
+      end
+
+      site.pages += index_pages.values
+    end
+  end
+
+  class TagsIndexPageGenerator < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      site.data['tags'] = []
+      index_pages = {}
+
+      site.tags.each do |tag, documents|
+        index = NewsIndexPage.new(
+          site,
+          site.config['tag_index_permalink'].gsub(/:tag/, tag),
+          { description: "#{tag} news listing: keep up to date on the latest #{site.config['title']} news.",
+            short_title: tag,
+            title: site.config['tag_index_title'].gsub(/:tag/, tag)
+          }
+        )
+
+        index_pages[index.url] = index
+        index_pages[index.url].data['documents'] = documents
+        site.data['tags'] << index
+      end
+
+      site.pages += index_pages.values
+    end
+  end
+
+  class LocalNavGenerator < Generator
+    safe true
+    priority :high
+
+    def generate(site)
+      site.data['local_nav'] = {}
+
+      site.each_site_file do |f|
+        if f.is_a?(Jekyll::Document) or f.html?
+          local_nav = site.data['local_nav']
+
+          f.url.split(/\//).each do |token|
+            if token.end_with?('.html')
+              local_nav[token] = f
+            else
+              token += '/'
+
+              local_nav[token] = {} unless local_nav.key?(token)
+
+              local_nav = local_nav[token]
+            end
+          end
         end
       end
     end
--- _site/_plugins/tags.rb
+++ _site/_plugins/tags.rb
@@ -12,6 +12,84 @@ module Jekyll
       end
     end
 
+    class LocalNavTag < Liquid::Tag
+      def initialize(tag_name, markup, parse_context)
+        markup.strip!.gsub!(/\\([{}])/, '\1')
+        super
+      end
+
+      def render(context)
+        site = context.registers[:site]
+        local_nav = site.data['local_nav']
+        page = site.data['pages'][context.registers[:page]['url']]
+        directories = (page.data['local_nav'] || File.dirname(page.url)).split(/\//)
+
+        directories.push('') if directories.empty?
+
+        directories.each do |token|
+          token += '/'
+          local_nav = local_nav[token]
+        end
+
+        render_local_nav(local_nav, page) unless local_nav.nil?
+      end
+
+      private
+
+        def render_local_nav(links, page)
+          sort_local_nav(links).reduce('') do |html, link|
+            if link.is_a?(Hash)
+              html += Liquid::Template.parse(@markup).render({
+                'link'=> link['index.html'],
+                'page'=> page,
+                'section'=> render_local_nav(link, page)
+              }) + "\n"
+            elsif link.is_a?(Jekyll::Document) or not link.index?
+              html += Liquid::Template.parse(@markup).render({
+                'link'=> link,
+                'page'=> page
+              }) + "\n"
+            else
+              html
+            end
+          end
+        end
+
+        def sort_local_nav(links)
+          links.values
+            .sort { |a, b|
+              a = a['index.html'] if a.is_a?(Hash)
+              b = b['index.html'] if b.is_a?(Hash)
+
+              begin
+                if a.data['local_nav_weight'] < b.data['local_nav_weight']
+                  -1
+                elsif a.data['local_nav_weight'] > b.data['local_nav_weight']
+                  1
+                else
+                  0
+                end
+              rescue
+                0
+              end
+            }
+        end
+    end
+
+    class ShortTitleTag < Liquid::Tag
+      def initialize(tag_name, markup, parse_context)
+        markup.strip!
+        super
+      end
+
+      def render(context)
+        page = context[@markup]
+        page['short_title'] || page['title'] || page['name']
+      end
+    end
+
     Liquid::Template.register_tag('heading', Jekyll::Tags::HeadingTag)
+    Liquid::Template.register_tag('local_nav', Jekyll::Tags::LocalNavTag)
+    Liquid::Template.register_tag('short_title', Jekyll::Tags::ShortTitleTag)
   end
 end
